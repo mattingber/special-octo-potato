23358f487a7258d89eea99d158aed4368f0bbc3b
elad biran <eladex@gmail.com>
2021-06-08 15:49:37 +0300

    add hierarchy helper class

---
+++ src/shared/Hierarchy.ts
@@ -0,0 +1,30 @@
+type HierarchyOpts = {
+  separator: string
+}
+
+const defaultOpts: HierarchyOpts = {
+  separator: '/',
+};
+
+export class Hierarchy {
+  private _separator: string;
+  private h_splitted: string[];
+
+  private constructor(value: string, opts: HierarchyOpts) {
+    this._separator = opts.separator;
+    this.h_splitted = value === '' ? [] 
+      : value.split(this._separator);
+  }
+
+  public concat(hierarchy: Hierarchy) {
+    this.h_splitted.push(...hierarchy.h_splitted);
+  }
+
+  public value() {
+    return this.h_splitted.join(this._separator);
+  }
+
+  public static create(value: string, opts = defaultOpts) {
+    return new Hierarchy(value, opts);
+  }
+}
