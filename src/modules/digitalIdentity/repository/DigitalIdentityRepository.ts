71bb05d4144a90479cc7038ec7556595be173b95
elad biran <eladex@gmail.com>
2021-06-20 16:08:05 +0300

    feat(DI): add repository interface

---
+++ src/modules/digitalIdentity/repository/DigitalIdentityRepository.ts
@@ -0,0 +1,10 @@
+import { Repository } from "../../../core/infra/Repository";
+import { DigitalIdentity } from "../domain/DigitalIdentity";
+import { DigitalIdentityId } from "../domain/DigitalIdentityId";
+import { EntityId } from "../../entity/domain/EntityId";
+
+export interface DigitalIdentityRepository extends Repository<DigitalIdentity> {
+  save(digitalIdentity: DigitalIdentity): Promise<void>;
+  getByUniqueId(uniqueId: DigitalIdentityId): Promise<DigitalIdentity>;
+  getByEntityId(entityId: EntityId): Promise<DigitalIdentity>;
+}
