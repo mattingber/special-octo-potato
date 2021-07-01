9540430e246a4e100552b977cb627ed053229226
elad biran <eladex@gmail.com>
2021-07-01 12:59:37 +0300

    feat(Entity): implement 'updateDetails' method

--- src/modules/entity/domain/Entity.ts
+++ src/modules/entity/domain/Entity.ts
@@ -3,8 +3,9 @@ import { EntityId } from "./EntityId";
 import { Hierarchy } from "../../../shared/Hierarchy";
 import { has } from "../../../utils/ObjectUtils";
 import { Result, err, ok } from "neverthrow";
-import { Error } from "mongoose";
 import { IllegalEntityStateError } from "./errors/IllegalEntityStateError";
+import { AppError } from "../../../core/logic/AppError";
+import { CannotChangeEntityTypeError } from "./errors/CannotChangeEntityTypeError";
 
 enum EntityType {
   Soldier = 'soldier',
@@ -17,26 +18,7 @@ enum Sex {
   Female = 'female'
 }
 
-// type IllegalOperationError
-// interface EntityState {
-//   firstName: string;
-//   lastName?: string;
-//   entityType: EntityType;
-//   hierarchy?: Hierarchy;
-//   personalNumber?: string; // use value object
-//   identityCard?: string;
-//   rank?: string; //use vale object / enum
-//   akaUnit?: string;
-//   clearance: number; // value object
-//   mail?: string; //value object
-//   sex?: Sex;
-//   serviceType?: string; //value object
-//   dischargeDate?: Date;
-//   birthDate?: Date;
-//   jobTitle?: string;
-//   address?: string; // value?
-// }
-
+// todo: add displayName
 
 type CommonEntityProps = {
   firstName: string;
@@ -89,6 +71,7 @@ type EntityState = {
   lastName?: string;
   entityType: EntityType;
   hierarchy?: Hierarchy;
+  displayName?: string;
   personalNumber?: string; // use value object
   identityCard?: string;
   rank?: string; //use vale object / enum
@@ -106,257 +89,196 @@ type EntityState = {
   goalUserId?: string;
 }
 
-type CommonState = Pick<EntityState, 'firstName' | 'entityType' | 'hierarchy' | 'clearance' | 'mail' | 'jobTitle'>
+type CommonState = Pick<EntityState, 'firstName' | 'entityType' | 'hierarchy' | 'clearance' | 'mail' | 'jobTitle'>;
 
-type PersonState = 
-  CommonState &
-  Required<Pick<EntityState, 'lastName' | 'serviceType'>> &
-  Pick<EntityState, 'sex' | 'address' | 'dischargeDate' | 'birthDate' | 'phone' | 'mobilePhone'>;
+const REQUIRED_COMMON_FIELDS: (keyof EntityState)[] = ['firstName', 'entityType'];
 
-type SoldierState = 
-  PersonState & 
-  Required<Pick<EntityState, 'personalNumber'>> & 
-  Pick<EntityState, 'rank' | 'identityCard' | 'akaUnit'>;
+// type PersonState = 
+//   CommonState &
+//   Required<Pick<EntityState, 'lastName' | 'serviceType'>> &
+//   Pick<EntityState, 'sex' | 'address' | 'dischargeDate' | 'birthDate' | 'phone' | 'mobilePhone'>;
 
-type CivilianState = 
-  PersonState & 
-  Required<Pick<EntityState, 'identityCard'>> & 
-  Pick<EntityState, 'personalNumber'>;
+const REQUIRED_PERSON_FIELDS: (keyof EntityState)[] = ['firstName', 'lastName', 'serviceType'];
 
-type UpdateDto = Partial<Omit<EntityState, 'hierarchy' | 'phone' | 'mobilePhone'> & {
-  phone: string[];
-  mobilePhone: string[]
-}>;
+// type SoldierState = 
+//   PersonState & 
+//   Required<Pick<EntityState, 'personalNumber'>> & 
+//   Pick<EntityState, 'rank' | 'identityCard' | 'akaUnit'>;
 
-  /**
-   firstName: string;
-  entityType: EntityType;
-  hierarchy?: Hierarchy;
-  clearance?: number; // value object
-  mail?: string; //value object, should be required??
-  jobTitle?: string;
-   */
+// type CivilianState = 
+//   PersonState & 
+//   Required<Pick<EntityState, 'identityCard'>> &  
+//   Pick<EntityState, 'personalNumber' | 'rank'>;
 
-// type yy =SoldierEntityProps & CivilianEntityProps
+  
+const ENTITY_TYPE_VALID_STATE: {
+[k in EntityType]: {
+  required: (keyof EntityState)[],
+  forbidden: (keyof EntityState)[],
+}
+} = {
+  [EntityType.Civilian]: {
+    required: [...REQUIRED_PERSON_FIELDS, 'identityCard'],
+    forbidden: ['goalUserId'],
+  },
+  [EntityType.Soldier]: {
+    required: [...REQUIRED_PERSON_FIELDS, 'personalNumber'],
+    forbidden: ['goalUserId'],
+  },
+  [EntityType.GoalUser]: {
+    required: ['firstName', 'goalUserId'],
+    forbidden: ['identityCard', 'rank', 'serviceType', 'sex', 'address', 'dischargeDate', 'birthDate'],
+  },
+};
 
-// const s: yy = {
+const SET_ONLY_ONCE_FIELDS = new Set(['sex', 'identityCard', 'personalNumber', 'birthDate'] as (keyof EntityState)[]);
 
-// }
-
-
+type UpdateDto = Partial<Omit<EntityState, 'hierarchy' | 'displayName'>>;
+type UpdateResult = Result<
+  void,
+  IllegalEntityStateError |
+  CannotChangeEntityTypeError |
+  AppError.CannotUpdateFieldError
+>
 
 export class Entity extends AggregateRoot {
-  // private _firstName: string;
-  // private _lastName: string;
-  // private _entityType: EntityType;
-  // private _hierarchy?: Hierarchy;
-  // private _rank?: string;
-  // private _identityCard?: string; 
-  // private _personalNumber?: string;
-  // private _akaUnit?: string;
-  // private _clearance?: number;
-  // private _mail?: string; // DI
-  // private _sex?: Sex;
-  // private _serviceType?: string; //value object
-  // private _dischargeDate?: Date;
-  // private _birthDate?: Date;
-  // private _jobTitle?: string; // 
-  // private _address?: string; // 
-
+ 
   private _state: EntityState;
-
-  private static readonly_fields: Set<keyof EntityState> = new Set(
-    ['sex', 'identityCard', 'personalNumber', 'birthDate'] as (keyof EntityState)[]
-  )
 
   private constructor(id: EntityId, props: EntityState) {
     super(id);
     this._state = props;
-    // this._firstName = props.firstName;
-    // this._lastName = props.lastName || '';
-    // this._entityType = props.entityType;
-    // this._hierarchy = props.hierarchy;
-    // this._rank = props.rank;
-    // this._identityCard = props.identityCard;
-    // this._personalNumber = props.personalNumber;
-    // this._akaUnit = props.akaUnit;
-    // this._clearance = props.clearance;
-    // this._sex = props.sex;
-    // this._serviceType = props.serviceType;
-    // this._dischargeDate = props.dischargeDate;
-    // this._birthDate = props.birthDate;
-    // this._jobTitle = props.jobTitle;
-    // this._address = props.address;
   }
 
-  // public setIdentityCard(identityCard: string) {
-  //   if (this._entityType === EntityType.GoalUser) {
-  //     return; //error
-  //   }
-  //   if (!!this._identityCard) {
-  //     return; // error
-  //   }
-  //   this._identityCard = identityCard;
-  // }
-
-  // public setPersonalNumber(personalNumber: string) {
-  //   if (this._entityType === EntityType.GoalUser) {
-  //     return; //error
-  //   }
-  //   if (!!this._personalNumber) {
-  //     return; // error
-  //   }
-  //   this._personalNumber = personalNumber;
-  // }
-
-  // public setHierarchy(hierarchy: Hierarchy) {
-  //   this._hierarchy = hierarchy;
-  // }
+  public setHierarchy(hierarchy: Hierarchy) {
+    this._state.hierarchy = hierarchy;
+  }
 
   /**
    * todo: are undefined fields should be ignored?
+   * phone should override or add ?
    * @param updateDto 
    */
-  public updateDetails(updateDto: UpdateDto) {
+  public updateDetails(updateDto: UpdateDto): UpdateResult {
+    // check if the key is readonly and already has been set
     for(const f of Object.keys(updateDto)) {
-      // check if the key is readonly and already has been set
       if (
-        f in Entity.readonly_fields && 
+        f in SET_ONLY_ONCE_FIELDS && 
         has(this._state, f as keyof UpdateDto)
       ){
-        // error
+        return err(AppError.CannotUpdateFieldError.create(f));
       }
     }
-    if ( // check for illegal 'entityType' transition
-      this._state.entityType !== EntityType.GoalUser && 
-      updateDto.entityType === EntityType.GoalUser
+    // check for illegal 'entityType' transition
+    if(
+      has(updateDto, 'entityType') &&
+      !Entity.isValidEntityTypeTransition(this._state.entityType, updateDto.entityType)
     ) {
-      // error
+      return err(CannotChangeEntityTypeError.create(this._state.entityType, updateDto.entityType));
     }
     const newState = { ...this._state, ...updateDto };
-
-
-    
-
-
+    const isValid = Entity.isValidEntityState(newState);
+    if(isValid.isOk()) {
+      this._state = newState;
+    }
+    return isValid;
   }
 
-  private changeToCivilian() {
-
-  }
-
-  private static isValidSoldierProps(props: EntityState): props is CreateSoldierProps {
-    const { entityType, personalNumber } = props;
-    return Entity.isValidPerson(props) 
-      && entityType === EntityType.Soldier 
-      && !!personalNumber;
-  }
-
-  private static isValidCivilianProps(props: EntityState): props is CreateCivilianProps {
-    const { identityCard, entityType } = props;
-    return Entity.isValidPerson(props) 
-      && entityType === EntityType.Civilian 
-      && !!identityCard;
-  }
-
-  private static isValidPersonState(props: EntityState): Result<void, IllegalEntityStateError> {
-    if(!has())
-  }
-
-  private static isValidGoalUserState(state: EntityState): Result<void, Error> {
-    return (
-      state.entityType === EntityType.GoalUser &&
-      has(state, 'firstName') && 
-      has(state, 'goalUserId')
-    );
-  }
-
-  private static isValidCivilianState(state: EntityState): Result<void, IllegalEntityStateError> {
-    if (!has(state, 'identityCard')) {
-      return err(IllegalEntityStateError.create('missing identity card field'));
+  private static isValidEntityState(state: EntityState): 
+    Result<void, IllegalEntityStateError> {
+    // entity has all common required fields
+    for(const k of REQUIRED_COMMON_FIELDS) {
+      if(!has(state, k)) {
+        return err(IllegalEntityStateError.create(`entity missing required field: ${k}`));
+      }
+    }
+    // entity has all required fields for it's type
+    const { required, forbidden } = ENTITY_TYPE_VALID_STATE[state.entityType]
+    for(const k of required) {
+      if(!has(state, k)) {
+        return err(IllegalEntityStateError.create(`${state.entityType} missing required field: ${k}`));
+      }
+    }
+    // entity ***
+    for(const k of forbidden) {
+      if(has(state, k)) {
+        return err(IllegalEntityStateError.create(`${state.entityType} cannot have field: ${k}`));
+      }
     }
     return ok(undefined);
   }
 
-  private static createSoldier(id: EntityId, props: CreateSoldierProps) {
-    return new Entity(id, props);
+  private static isValidEntityTypeTransition(from: EntityType, to: EntityType): boolean {
+    if (
+      from !== to && (
+      from === EntityType.GoalUser ||
+      to === EntityType.GoalUser)
+    ) {
+      return false;
+    }
+    return true;
   }
 
-  private static createCivilian(id: EntityId, props: CreateCivilianProps) {
-    return new Entity(id, props);
-  }
-
-  static create(id: EntityId, props: EntityState, opts: CreateOpts) {
-    let entity: Entity | null = null;
-    switch(props.entityType) {
-      case EntityType.Soldier: 
-        if (Entity.isValidSoldierProps(props)) {
-          entity = Entity.createSoldier(id, props);
-        }
-        break;
-      case EntityType.Civilian: 
-        if (Entity.isValidCivilianProps(props)) {
-          entity = Entity.createCivilian(id, props);
-        }
-        break;
-      default:
-        entity = null
+  static create(id: EntityId, state: EntityState, opts: CreateOpts): Result<Entity, IllegalEntityStateError> {
+    const isValid = Entity.isValidEntityState(state);
+    if (isValid.isOk()) {
+      return ok(new Entity(id, state));
     }
-    if (!!entity) {
-      return entity;
-    }
+    return err(isValid.error);
   }
 
   get entityId() {
     return EntityId.create(this.id.toValue());
   }
-
   get name() {
     return {
-      firstName: this._firstName,
-      lastName: this._lastName
+      firstName: this._state.firstName,
+      lastName: this._state.lastName
     }
   }
   get entityType() {
-    return this._entityType;
+    return this._state.entityType;
   }
   get personalNumber() {
-    return this._personalNumber;
+    return this._state.personalNumber;
   }
   get identityCard() {
-    return this._identityCard;
+    return this._state.identityCard;
   }
   get rank() {
-    return this._rank;
+    return this._state.rank;
   }
   get akaUnit() {
-    return this._akaUnit;
+    return this._state.akaUnit;
   }
   get hierarchy() {
-    return this._hierarchy;
+    return this._state.hierarchy;
   }
   get clearance() {
-    return this._clearance;
+    return this._state.clearance;
   }
   get sex() {
-    return this._sex;
+    return this._state.sex;
   }
   get serviceType() {
-    return this._serviceType;
+    return this._state.serviceType;
   }
   get dischargeDate() {
-    return this._dischargeDate;
+    return this._state.dischargeDate;
   }
   get birthDate() {
-    return this._birthDate;
+    return this._state.birthDate;
   }
   get jobTitle() {
-    return this._jobTitle;
+    return this._state.jobTitle;
   }
   get address() {
-    return this._address;
+    return this._state.address;
   }
   get mail() {
-    return this._mail;
+    return this._state.mail;
+  }
+  get displayName() {
+    return this._state.displayName;
   }
 }
