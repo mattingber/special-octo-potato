039b6d90dfcd1c70764fee8f8c9d019ceb698d2f
elad biran <eladex@gmail.com>
2021-07-12 16:16:47 +0300

    refactor(Role): save aggregate events when persisting it (repo save)

--- src/modules/Role/infra/mongoose/RoleRepository.ts
+++ src/modules/Role/infra/mongoose/RoleRepository.ts
@@ -5,11 +5,13 @@ import { RoleId } from "../../domain/RoleId";
 import { RoleRepository as IRoleRepository } from "../../repository/RoleRepository"
 import { RoleMapper as Mapper} from "./RoleMapper";
 import { RoleDoc } from "./RoleModel";
+import { Outbox } from "../../../../shared/infra/mongoose/eventOutbox/OutBox";
 
 export class RoleRepository implements IRoleRepository {
 
   constructor(
-    private _model: Model<RoleDoc> 
+    private _model: Model<RoleDoc>,
+    private _outbox: Outbox,
   ) {}
 
   async getByRoleId(roleId: RoleId): Promise<Role | null> {
@@ -28,10 +30,15 @@ export class RoleRepository implements IRoleRepository {
 
   async save(role: Role) {
     const persistanceState = Mapper.toPersistance(role);
-    await this._model.updateOne(
-      { roleId: persistanceState.roleId }, 
-      persistanceState, 
-      { upsert: true }
-    );
+    const session = await this._model.startSession();
+    await session.withTransaction(async () => {
+      await this._model.updateOne(
+        { roleId: persistanceState.roleId }, 
+        persistanceState, 
+        { upsert: true }
+      ).session(session);
+      await this._outbox.put(role.domainEvents, session);
+    });
+    session.endSession();
   }
 }
