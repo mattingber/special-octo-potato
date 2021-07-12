04fa33d6c3b07175860a52e968f64f15ec51a8e6
elad biran <eladex@gmail.com>
2021-07-12 16:14:39 +0300

    feat(core): add domain events in aggregate abstract superclass

--- src/core/domain/AggregateRoot.ts
+++ src/core/domain/AggregateRoot.ts
@@ -1,8 +1,18 @@
 import { EntityBase } from "./EntityBase";
 import { EntityId } from "../../modules/entity/domain/EntityId";
+import { IDomainEvent } from "./event/IDomainEvent";
 
 export abstract class AggregateRoot extends EntityBase {
-  // todo: add events and optimistic concurrency control
+  private _domainEvents: IDomainEvent[] = [];
+
+  protected addDomainEvent(event: IDomainEvent) {
+    this._domainEvents.push(event);
+  }
+
+  get domainEvents() {
+    return [...this._domainEvents];
+  }
+
 }
 
 export type CreateOpts = {
