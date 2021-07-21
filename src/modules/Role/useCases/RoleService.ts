160662fdd52b9e548427f5476120bd45fac0b170
elad biran <eladex@gmail.com>
2021-07-21 14:25:49 +0300

    chore: add roleService

--- src/modules/Role/useCases/RoleService.ts
+++ src/modules/Role/useCases/RoleService.ts
@@ -6,11 +6,15 @@ import { Source } from "../../digitalIdentity/domain/Source";
 import { Result, err, ok } from "neverthrow";
 import { AppError } from "../../../core/logic/AppError";
 import { GroupId } from "../../group/domain/GroupId";
+import { ConnectDigitalIdentityDTO } from "./dtos/ConnectDigitalIdentityDTO";
+import { DigitalIdentityRepository } from "../../digitalIdentity/repository/DigitalIdentityRepository";
+import { DigitalIdentityId } from "../../digitalIdentity/domain/DigitalIdentityId";
 
 export class RoleService {
   constructor(
     private roleRepository: RoleRepository,
-    private groupRepository: GroupRepository
+    private groupRepository: GroupRepository,
+    private diRepository: DigitalIdentityRepository
   ){}
 
   async createRole(createRoleDTO: CreateRoleDTO): Promise<Result<
@@ -32,6 +36,37 @@ export class RoleService {
       source: sourceOrError.value,
       jobTitle: createRoleDTO.jobTitle,
     });
+    await this.roleRepository.save(role);
+    return ok(undefined);
+  }
+
+  async connectDigitalIdentity(connectDTO: ConnectDigitalIdentityDTO): Promise<Result<
+    void, 
+    AppError.ValueValidationError |
+    AppError.ResourceNotFound |
+    any // any role entity errors
+  >> {
+    const roleId = RoleId.create(connectDTO.roleId);
+    const idOrError = DigitalIdentityId.create(connectDTO.digitalIdentityUniqueId)
+      .mapErr(msg => AppError.ValueValidationError.create(msg));
+    if(idOrError.isErr()) {
+      return err(idOrError.error);
+    }
+    const role = await this.roleRepository.getByRoleId(roleId);
+    if(!role) {
+      return err(AppError.ResourceNotFound.create(connectDTO.roleId, 'role id'))
+    }
+    const di = await this.diRepository.getByUniqueId(idOrError.value);
+    if(!di) {
+      return err(AppError.ResourceNotFound.create(
+        connectDTO.digitalIdentityUniqueId, 
+        'digitalIdentity UniqueId'
+      ));
+    }
+    const res = role.connectDigitalIdentity(di);
+    if(res.isErr()) {
+      return err(res.error);
+    }
     await this.roleRepository.save(role);
     return ok(undefined);
   }
---
+++ src/modules/Role/useCases/dtos/ConnectDigitalIdentityDTO.ts
@@ -0,0 +1,4 @@
+export type ConnectDigitalIdentityDTO = {
+  roleId: string;
+  digitalIdentityUniqueId: string;
+}
