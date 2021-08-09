bb5af42bd4ba2813557ba34d1c670f056de53b00
eladex <eladex@gmail.com>
2021-08-09 21:48:04 +0300

    feat(Group): add group controller

---
+++ src/modules/group/infra/http/GroupController.ts
@@ -0,0 +1,64 @@
+import { GroupService } from "../../useCases/GroupService";
+import { Request, Response } from "express";
+import {
+  CreateGroupDTO,
+  joiSchema as CreateGroupSchema
+} from '../../useCases/dto/CreateGroupDTO';
+import {
+  MoveGroupDTO,
+  joiSchema as MoveGroupSchema
+} from '../../useCases/dto/MoveGroupDTO';
+import { ResponseHandler } from "../../../../shared/infra/http/helpers/BaseController";
+import { AppError } from "../../../../core/logic/AppError";
+import { CreateRoleDTO } from "../../../Role/useCases/dtos/CreateRoleDTO";
+
+
+export class GroupController {
+  constructor(
+    private _groupService: GroupService
+  ){}
+
+  /**
+   * POST /groups
+   */
+  async createGroup(req: Request, res: Response) {
+    const { error, value: dto } = CreateGroupSchema.validate(req.body);
+    if(!!error) {
+      return ResponseHandler.clientError(res, error.message);
+    }
+    const result = await this._groupService.createGroup(dto as CreateGroupDTO);
+    if(result.isErr()) {
+      return ResponseHandler.clientError(res, result.error.message);
+    }
+    return ResponseHandler.ok(res);
+  }
+
+  /**
+   * PUT /groups/:id/parent/:parentId
+   * @param req 
+   * @param res 
+   * @returns 
+   */
+  async moveGroup(req: Request, res: Response) {
+    const { error, value: dto } = CreateGroupSchema.validate({
+      groupId: req.params.id,
+      parentId: req.params.parentId,
+    });
+    if(!!error) {
+      return ResponseHandler.clientError(res, error.message);
+    }
+    const result = await this._groupService.createGroup(dto as CreateGroupDTO);
+    if(result.isErr()) {
+      const err = result.error;
+      if( // only if the group itself is not found return 404
+        err instanceof AppError.ResourceNotFound &&
+        err.resource === (dto as MoveGroupDTO).groupId
+      ) {
+        return ResponseHandler.notFound(res, err.message);
+      } 
+      // else return 400 
+      return ResponseHandler.clientError(res, result.error.message);
+    }
+    return ResponseHandler.ok(res);
+  }
+}
