43fe6f03e0926241b4ed0ff533e2897316fa3cf7
eladex <eladex@gmail.com>
2021-08-09 15:30:51 +0300

    feat(utils): add toArray util function

--- src/utils/arrayUtils.ts
+++ src/utils/arrayUtils.ts
@@ -1,3 +1,5 @@
 export const filterNullOrUndefined = <T>(arr: (T | undefined | null)[]): T[] => {
   return arr.filter(v => v !== undefined && v !==null) as T[];
 }
+
+export const toArray = <T>(val: T | T[]) => Array.isArray(val) ? val : [val];
