c1d95a2cbe9ed6b609cd6d4aff19d04796347df9
eladex <eladex@gmail.com>
2021-08-09 17:40:21 +0300

    refactor(DI): add joi schemas to DTOs

--- src/modules/digitalIdentity/useCases/dtos/CreateDigitalIdentityDTO.ts
+++ src/modules/digitalIdentity/useCases/dtos/CreateDigitalIdentityDTO.ts
@@ -1,7 +1,17 @@
+import Joi from 'joi';
+
 export type CreateDigitalIdentityDTO = {
   uniqueId: string;
   type: string;
   source: string;
   mail?: string;
-  canConnectRole?: boolean;
+  isRoleAttachable?: boolean;
 }
+
+export const joiSchema = Joi.object({
+  uniqueId: Joi.string().required(),
+  type: Joi.string().required(),
+  source: Joi.string().required(),
+  mail: Joi.string(),
+  isRoleAttachable: Joi.boolean(),
+});
--- src/modules/digitalIdentity/useCases/dtos/UpdateDigitalIdentityDTO.ts
+++ src/modules/digitalIdentity/useCases/dtos/UpdateDigitalIdentityDTO.ts
@@ -1,5 +1,13 @@
+import Joi from 'joi';
+
 export type UpdateDigitalIdentityDTO = {
   uniqueId: string
   mail?: string;
   isRoleAttachable?: boolean;
 }
+
+export const joiSchema = Joi.object({
+  uniqueId: Joi.string().required(),
+  mail: Joi.string(),
+  isRoleAttachable: Joi.boolean(),
+});
