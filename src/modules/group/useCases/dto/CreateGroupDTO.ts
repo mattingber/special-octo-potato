03ed2974ada12ebd43e5efc9b3aa5f3fc50e4b62
eladex <eladex@gmail.com>
2021-08-09 21:47:47 +0300

    feat(Group): add joi schemas to DTOS

--- src/modules/group/useCases/dto/CreateGroupDTO.ts
+++ src/modules/group/useCases/dto/CreateGroupDTO.ts
@@ -1,6 +1,16 @@
+import Joi from 'joi';
+
 export type CreateGroupDTO = {
   name: string;
   source: string;
   parentId?: string;
   akaUnit?: string;
 }
+
+export const joiSchema = Joi.object({
+  name: Joi.string().required(),
+  source: Joi.string().required(),
+  parentId: Joi.string(),
+  akaUnit: Joi.string(),
+});
+
--- src/modules/group/useCases/dto/MoveGroupDTO.ts
+++ src/modules/group/useCases/dto/MoveGroupDTO.ts
@@ -1,4 +1,11 @@
+import Joi from 'joi';
+
 export type MoveGroupDTO = {
   groupId: string;
   parentId: string;
 }
+
+export const joiSchema = Joi.object({
+  groupId: Joi.string().required(),
+  parentId: Joi.string().required(),
+});
