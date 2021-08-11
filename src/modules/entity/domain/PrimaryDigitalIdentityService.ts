50e8242512829c1a7d23e964338ae40d42eb018d
eladex <eladex@gmail.com>
2021-08-12 00:53:41 +0300

    feat(Entity): add primary DI chooser domain service

---
+++ src/modules/entity/domain/PrimaryDigitalIdentityService.ts
@@ -0,0 +1,107 @@
+import { Entity } from "./Entity";
+import { DigitalIdentity, DigitalIdentityType } from "../../digitalIdentity/domain/DigitalIdentity";
+import { Role } from "../../Role/domain/Role";
+import { Source } from "../../digitalIdentity/domain/Source";
+import { Mail } from "../../digitalIdentity/domain/Mail";
+import { EntityId } from "./EntityId";
+import { DigitalIdentityId } from "../../digitalIdentity/domain/DigitalIdentityId";
+
+type DigitalIdentityObject = {
+  type: DigitalIdentityType;
+  source: Source;
+  mail?: Mail; // use value Object
+  entityId: EntityId;
+  canConnectRole?: boolean;
+  uniqueId: DigitalIdentityId;
+};
+
+const OT_STRONG_SOURCE = 'OT';
+
+export class PrimaryDigitalIdentityService {
+  private _primarySourceMap: Map<string, Source>;
+
+  constructor() {
+    this._primarySourceMap = new Map(); // TODO: build map from config
+  }
+
+  private static haveStrongSource(digitalIdentity: DigitalIdentityObject) {
+    return digitalIdentity.source.value === OT_STRONG_SOURCE;
+  }
+
+  private havePrimarySource(entity: Entity, digitalIdentity: DigitalIdentityObject) {
+    return !!entity.akaUnit ? 
+      digitalIdentity.source.value === this._primarySourceMap.get(entity.akaUnit)?.value :
+      false;
+  }
+
+  // public setEntityPrimaryDigitalIdentity(
+  //   entity: Entity,
+  //   connectedDigitalIdentity: DigitalIdentity,
+  // ) {
+  //   if (
+  //     !connectedDigitalIdentity.connectedEntityId?.equals(entity.entityId) ||
+  //     !connectedRole.digitalIdentityUniqueId?.equals(connectedDigitalIdentity.uniqueId)
+  //   ) {
+  //     /**
+  //      * DI not Connected to Entity or Role not Connected to DI 
+  //      * should return error so calling service will log it
+  //      */
+  //     return;
+  //   }
+  //   if (this.havePrimarySource(entity, connectedDigitalIdentity)) {
+  //     entity.setHierarchy(Hierarchy.create(connectedRole.hierarchy));
+  //     // set other fields
+  //   }
+  // }
+
+  public choosePrimaryDigitalIdentity(entity: Entity, digitalIdentities: DigitalIdentityObject[]) {
+    const connected = digitalIdentities.filter(di => di.entityId.equals(entity.entityId));
+    // no connected DIs, set primary to undefined
+    if(connected.length === 0) {
+      entity.updateDetails({ primaryDigitalIdentityId: undefined });
+      return;
+    }
+    // check if current primary has the strongest source
+    let currentPrimary = connected.find(di => di.uniqueId.equals(entity.primaryDigitalIdentityId));
+    if( 
+      !!currentPrimary && 
+      PrimaryDigitalIdentityService.haveStrongSource(currentPrimary)
+    ) {
+      return;
+    }
+    // find if one of the other DIs has the strongest source
+    const strongSourceDI = connected.find(PrimaryDigitalIdentityService.haveStrongSource);
+    if(!!strongSourceDI) {
+      entity.updateDetails({ primaryDigitalIdentityId: strongSourceDI.entityId });
+      return;
+    }
+    // check for primary source DI (and the current primary has not)
+    const primarySourceDI = connected.find(di => this.havePrimarySource(entity, di));
+    if(
+      (!currentPrimary || !this.havePrimarySource(entity, currentPrimary)) &&
+      !!primarySourceDI
+    ) {
+      entity.updateDetails({ primaryDigitalIdentityId: primarySourceDI.entityId });
+      return;
+    }
+    // connect one of the DIs
+    if(!currentPrimary) {
+      entity.updateDetails({ primaryDigitalIdentityId: connected[0].entityId });
+      return;
+    }
+    // else, the current primary
+   
+  }
+  /* 
+  happens on:
+    - Role connecting / disconneting to DI
+    - DI connecting / disconneting to Entity
+    - Group moving to new Parent 
+    - Role moving to new parent group
+  (Entity , DI, Role) =>
+    if DI is primary of Entity {
+      entity.hierarchy = Role.hierarchy
+      entity.X = Role.X
+    }
+  */
+}
