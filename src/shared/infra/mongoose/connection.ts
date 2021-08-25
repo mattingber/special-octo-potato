4d23101da085c1afa2a0b36028610c8cd1db2617
elad biran <eladex@gmail.com>
2021-08-25 12:22:14 +0300

    add dockerfile

--- src/shared/infra/mongoose/connection.ts
+++ src/shared/infra/mongoose/connection.ts
@@ -5,6 +5,7 @@ import config from "config";
 const opts: ConnectOptions = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useCreateIndex: true,
   poolSize: Number(config.get('db.mongo.poolSize')),
 };
 
